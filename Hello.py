8ba49db6f8cf1683a98c06117066b682597821ef
Arjun-9171 <arjunanand923@gmail.com>
2024-07-08 18:36:09 -0700

    added Labels and Buttons

--- Hello.py
+++ Hello.py
@@ -12,6 +12,14 @@ class Hello(App):
     
     self.window.add_widget(Image(source = 'logo.png'))
     
+    self.greetings = Label(text = 'what is your name?', font_size = 36, color = 'green')
+    self.window.add_widget(self.greetings)
+    
+    self.user_Input = TextInput(multiline = False, padding_y = (20, 20))
+    self.window.add_widget(self.user_Input)
+    
+    self.enter_Input = Button(text = 'Greet', color = 'black', background_color = 'white', bold = True, font_size = 40)
+    self.window.add_widget(self.enter_Input)
     return self.window
     
 Hello().run()
